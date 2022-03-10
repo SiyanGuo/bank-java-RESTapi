9cdc2cd5668ffe9075297baa7b158c855ed9a2f1
Serena Guo <syg9345@gmail.com>
2022-03-09 22:27:30 -0500

    optinization loggging information

--- bank-api/src/main/java/com/revature/main/Driver.java
+++ bank-api/src/main/java/com/revature/main/Driver.java
@@ -4,15 +4,20 @@ import com.revature.controller.ClientController;
 import com.revature.controller.Controller;
 import com.revature.controller.ExceptionController;
 import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class Driver {
+    public static Logger logger = LoggerFactory.getLogger(Driver.class);
+
     public static void main(String[] args) {
 
-        String url = System.getenv("db_password");
-        System.out.println(url);
-
         Javalin app = Javalin.create();
+        app.before((ctx) -> {
+            logger.info(ctx.method() + " request received for " + ctx.path());
+        });
+
         mapControllers(app, new ClientController(), new ExceptionController());
         app.start();
     }
